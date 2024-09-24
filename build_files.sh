eff623b7697fa3ed81e695daf95b9a956025f748
Srushti Shankar <139474378+srushtishankar@users.noreply.github.com>
2024-09-24 14:36:41 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,12 @@
+# build_files.sh
 echo "BUILD START"
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
+
+# Ensure the directory exists
+if [ ! -d "staticfiles_build" ]; then
+    echo "Error: staticfiles_build directory not found!"
+    exit 1
+fi
+
 echo "BUILD END"
